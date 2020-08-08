027eb88c880f7e19ef1fe7f392ad1faa670f43fe
song.tang@ucloud.cn <Tangsong1>
2020-08-08 16:39:11 +0800

    1.0.2

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// const log = (msg) => console.log(msg)
+
+// const shout = (msg) => {
+//     if (typeof msg === str) {
+//         console.log(msg.toUpperCase())
+//     } else {
+//         log(msg)
+//     };
+// };
+
+// const whisper = (msg) => {
+//     if (typeof msg === str) {
+//         console.log(msg.toUpperCase())
+//     } else {
+//         log(msg)
+//     };
+// };
+
+// module.exports = {log, shout, whisper}
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ webpack.md
@@ -0,0 +1,3 @@
+# 介绍 **Webpack** 初始化
+
+npm i -D install webpack vue-loader url-loader css-loader file-loader mini-css-extract-plugin webpack-cli webpack-dev-server
